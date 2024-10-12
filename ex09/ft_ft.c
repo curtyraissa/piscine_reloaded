d3acc5eb907afade3c673901b802dbd1909e235d
curtyraissa <raissacurty@gmail.com>
2024-10-12 18:33:31 +0100

    feat: implement ex09

---
+++ ex09/ft_ft.c
@@ -0,0 +1,14 @@
+void    ft_ft(int *nbr)
+{
+    *nbr = 42;
+}
+
+/*
+#include <stdio.h>
+int main(void) {
+    int number;
+    ft_ft(&number); // Passa o endereço de number para ft_ft
+    printf("%d\n", number); // Deve imprimir 42
+    return 0;
+}
+*/
