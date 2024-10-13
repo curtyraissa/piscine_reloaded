e0a2a77efb9b619ab43d2cbad0e82006a1afb3dc
curtyraissa <raissacurty@gmail.com>
2024-10-13 13:55:32 +0100

    feat: impement ex19

---
+++ ex19/ft_sort_params.c
@@ -0,0 +1,63 @@
+void ft_putchar(char c);
+
+int ft_strcmp(char *s1, char *s2)
+{
+    while (*s1 && *s2 && *s1 == *s2)
+    {
+        s1++;
+        s2++;
+    }
+    return (*s1 - *s2);
+}
+
+int ft_print_params(int argc, char **argv)
+{
+    int	i;
+    int j;
+
+    i = 1;
+    if(argc > 1)
+    {
+        while (i < argc)
+        {
+            j = 0;
+            while(argv[i][j])
+            {
+                ft_putchar(argv[i][j]);
+                j++;
+            }
+            ft_putchar('\n');
+            i++;
+        }
+    }
+    return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	char	*max;
+	int		s;
+
+	s = 1;
+	while (s < argc - 1)
+	{
+		if (ft_strcmp(argv[s], argv[s + 1]) > 0)
+		{
+			max = argv[s];
+			argv[s] = argv[s + 1];
+			argv[s + 1] = max;
+			s = 1;
+		}
+		else
+			s++;
+	}
+	ft_print_params(argc, argv);
+	return (0);
+}
+
+/*
+#include <unistd.h>
+void ft_putchar(char c) {
+    write(1, &c, 1);
+}
+*/
