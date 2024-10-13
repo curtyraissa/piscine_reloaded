e4241ca93cb18c72ad101e4fd1d1c67f37c802b7
curtyraissa <raissacurty@gmail.com>
2024-10-13 13:55:27 +0100

    feat: impement ex18

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,31 @@
+void ft_putchar(char c);
+
+int main(int argc, char **argv)
+{
+    int	i;
+    int j;
+
+    i = 0;
+    if(argc > 1)
+    {
+        while (i < argc)
+        {
+            j = 0;
+            while(argv[i][j])
+            {
+                ft_putchar(argv[i][j]);
+                j++;
+            }
+            ft_putchar('\n');
+            i++;
+        }
+    }
+    return (0);
+}
+
+/*
+#include <unistd.h>
+void ft_putchar(char c) {
+    write(1, &c, 1);
+}
+*/
