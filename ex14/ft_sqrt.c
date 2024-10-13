83c5a8f94d9d69bd93779dcc06666079acd5c1a1
curtyraissa <raissacurty@gmail.com>
2024-10-13 13:52:47 +0100

    feat: implement ex14

---
+++ ex14/ft_sqrt.c
@@ -0,0 +1,25 @@
+int ft_sqrt(int nb)
+{
+    int i;
+
+    i = 0;
+    if (nb < 0)
+        return (0);
+    while(i <= nb)
+    {
+        if(i * i == nb)
+            return (i);
+        i++;
+    }
+    return (0);
+}
+
+/*
+#include <stdio.h>
+int main(void) {
+    int number = 16;
+    int result = ft_sqrt(number);
+    printf("Raiz quadrada de %d é: %d\n", number, result);
+    return 0;
+}
+*/
