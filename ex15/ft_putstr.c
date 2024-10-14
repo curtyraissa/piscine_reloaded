d11556aef9c283f75ce1fad6f67d23b043376a12
curtyraissa <raissacurty@gmail.com>
2024-10-14 16:50:19 +0100

    fix: Norminette corrections and added header

--- ex15/ft_putstr.c
+++ ex15/ft_putstr.c
@@ -1,12 +1,24 @@
-void ft_putchar(char c);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcurty-g <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 13:12:52 by rcurty-g          #+#    #+#             */
+/*   Updated: 2024/10/14 13:35:22 by rcurty-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void ft_putstr(char *str)
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
 {
-    while(*str)
-    {
-        ft_putchar(*str);
-        str++;
-    }
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
 }
 
 /*
--- ex17/ft_strcmp.c
+++ ex17/ft_strcmp.c
@@ -1,11 +1,23 @@
-int ft_strcmp(char *s1, char *s2)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcurty-g <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 13:19:21 by rcurty-g          #+#    #+#             */
+/*   Updated: 2024/10/14 13:38:59 by rcurty-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(char *s1, char *s2)
 {
-    while (*s1 && *s2 && *s1 == *s2)
-    {
-        s1++;
-        s2++;
-    }
-    return (*s1 - *s2);
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
 
 /*
@@ -14,11 +26,9 @@ int main(void) {
     char *str1 = "Hello";
     char *str2 = "Hello";
     char *str3 = "World";
-
-    printf("Comparando '%s' e '%s': %d\n", str1, str2, ft_strcmp(str1, str2)); // Deve imprimir 0
-    printf("Comparando '%s' e '%s': %d\n", str1, str3, ft_strcmp(str1, str3)); // Deve imprimir um valor negativo
-    printf("Comparando '%s' e '%s': %d\n", str3, str1, ft_strcmp(str3, str1)); // Deve imprimir um valor positivo
-
+    printf("%d\n", ft_strcmp(str1, str2)); //0
+    printf("%d\n", ft_strcmp(str1, str3)); //negativo
+    printf("%d\n", ft_strcmp(str3, str1)); //positivo
     return 0;
 }
 */
