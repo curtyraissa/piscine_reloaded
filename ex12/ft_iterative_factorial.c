3af8ad1fbee1742a8bd37b450ddc698a9dc9f64a
curtyraissa <raissacurty@gmail.com>
2024-10-14 16:49:39 +0100

    fix: norminette corrections and added header

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -1,20 +1,32 @@
-int ft_iterative_factorial(int nb)
-{
-    int result;
-    int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcurty-g <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 13:10:04 by rcurty-g          #+#    #+#             */
+/*   Updated: 2024/10/14 13:31:03 by rcurty-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    result = 1;
-    i = 1;
-    if (nb < 0)
-        return (0);
-    if (nb == 0)
-        return (1);
-        while(i <= nb)
-        {
-            result *= i;
-            i++;
-        }
-        return (result);
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+	int	i;
+
+	result = 1;
+	i = 1;
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	while (i <= nb)
+	{
+		result *= i;
+		i++;
+	}
+	return (result);
 }
 
 /*
--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -1,10 +1,22 @@
-int ft_recursive_factorial(int nb)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcurty-g <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 13:10:42 by rcurty-g          #+#    #+#             */
+/*   Updated: 2024/10/14 13:32:21 by rcurty-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
 {
-    if (nb < 0)
-        return (0);
-    if (nb == 0)
-        return (1);
-    return (nb * ft_recursive_factorial(nb - 1));
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
 }
 
 /*
